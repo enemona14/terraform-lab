87e9f11baad2771c8acb2ab92b8e91cf51516701
Enemona <enemonag84@gmail.com>
2025-01-01 22:01:03 +0100

    add main.tf file

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-mona-443319"
+  credentials = file("project-mona-443319-c6c236f5c799.json")
+}
+
+resource "google_compute_instance" "test-vm" {
+  name         = "my-vm"
+  machine_type = "e2-micro"          
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = "default"
+
+    access_config {  
+    }
+  }
+}
